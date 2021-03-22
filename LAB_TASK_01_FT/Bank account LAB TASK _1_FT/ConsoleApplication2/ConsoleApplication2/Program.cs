9498f291570ca7b624f4906b171cb79eea378d3b
Your Name <naimulrohan789456123@gmail.com>
2021-03-22 13:07:42 +0600

    LAB TASK 1 DATE 3/22/21

---
+++ LAB_TASK_01_FT/Bank account LAB TASK _1_FT/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a1 = new Account("any name ", "id", 6000000);
+            Account a2 = new Account("any name", "id", 4000000);
+
+            a1.Withdraw(100000);
+            a1.Deposit(10000);
+            a2.Transfer(a2, 100000);
+            a1.ShowAllTransaction();
+            a2.ShowAllTransaction();
+        }
+    }
+
+}
